2c99d5a87dd7a4ad74eb0422371a7739c6c47fd4
jeffrpar <126530706+jeffrpar@users.noreply.github.com>
2023-07-10 22:24:37 -0500

    linked database to app

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const express = require("express");
 // Set Port
 const PORT = process.env.PORT || 3001;
 
+// reference database
+const dbjson = require('./db/db.json');
+
 // Creates new app with express
 const app = express();
 
@@ -18,6 +21,15 @@ app.get("/", (req, res) =>
 app.get("/notes", (req, res) =>
   res.sendFile(path.join(__dirname, "public/notes.html"))
 );
+
+// GET request for database
+app.get('/api/dbjson', (req, res) => {
+  // Log our request to the terminal
+  console.info(`${req.method} request received to get dbjson`);
+
+  // Sending databse to the client
+  return res.status(200).json(dbjson);
+});
 
 // listen on local server
 app.listen(PORT, () =>
